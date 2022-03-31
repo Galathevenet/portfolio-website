7922445d0b746ece6972119e0287543d290d6fca
Gala Thevenet <galathevenet@hotmail.com>
2022-03-31 11:21:35 +0200

    routes, controllers and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects, only: %i[index show] do
+    resources :users, only: %i[index]
+  end
 end
