a7330a272686d150b025dd5adb53bb04c68f052b
Gala Thevenet <galathevenet@hotmail.com>
2022-04-10 12:40:10 +0200

    added contact routes new and create + controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :projects, only: %i[index show] do
     resources :users, only: %i[index]
+    resources :contacts, only: [:new, :create]
   end
 end
