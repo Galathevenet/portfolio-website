5f4d4ba8fee20430cb04a143e15c89253ec3c9ce
Gala Thevenet <galathevenet@hotmail.com>
2022-04-18 15:27:43 +0200

    added dwnld button for my cv in desc card

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,8 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def download
+    send_file '/home/blog/downloads/away.png',:type=>"application/png", :x_sendfile=>true
+end
 end
